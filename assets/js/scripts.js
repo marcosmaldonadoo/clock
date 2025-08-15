1c6731688e469c4798b02e61cb93beba3ca8d5d1
marcosmaldonadoo <marcosmaldonado0708@gmail.com>
2025-08-15 08:05:59 -0300

    fix(script): corrigir troca de imagens de acordo com o horário

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,20 +1,20 @@
 function carregar(){
     var horario = document.getElementById('horario')
-    var imagem = document.getElementById('imagem')
+    var imagem = document.getElementById('foto')
     var mensagem = document.getElementById('mensagem')
     var data = new Date()
     var hora = data.getHours()
     horario.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12){
-        imagem.scr = 'assets/imagens/manha.png'
+        imagem.src = 'assets/img/fotomanha.png'
         document.body.style.background = '#e3b007'
         mensagem.innerHTML = 'Bom dia!'
     } else if (hora >= 12 && hora <= 18){
-        imagem.scr = 'assets/imagens/tarde.png'
+        imagem.src = 'assets/img/fototarde.png'
         document.body.style.background = '#c06b2e'
         mensagem.innerHTML = 'Boa tarde!'   
     } else {
-        imagem.scr = 'assets/imagens/noite.png'
+        imagem.src = 'assets/img/fotonoite.png'
         document.body.style.background = '#0c3d83'
         mensagem.innerHTML = 'Boa noite!'
     }
